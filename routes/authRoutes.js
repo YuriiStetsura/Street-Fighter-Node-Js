678bdfb5e8908f11130407599ca34ae117f84d71
YuriiStetsura <y.stetsura@gmail.com>
2021-05-11 08:41:39 +0300

    starter

---
+++ routes/authRoutes.js
@@ -0,0 +1,20 @@
+const { Router } = require('express');
+const AuthService = require('../services/authService');
+const { responseMiddleware } = require('../middlewares/response.middleware');
+
+const router = Router();
+
+
+router.post('/login', (req, res, next) => {
+    try {
+        const data = AuthService.login(req.body)       
+
+        res.data = data;
+    } catch (err) {
+        res.err = err;
+    } finally {
+        next();
+    }
+}, responseMiddleware);
+
+module.exports = router;
